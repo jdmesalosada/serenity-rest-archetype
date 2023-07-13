51295c70ee05f4dad7ebb582f64d7063d3b4552f
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2023-07-13 16:06:23 -0500

    added sample with lombok

--- src/test/java/starter/features/users/GetUsers.java
+++ src/test/java/starter/features/users/GetUsers.java
@@ -1,8 +1,9 @@
-package starter.users;
+package starter.features.users;
 
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.TestsRequirement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -14,16 +15,29 @@ import starter.questions.AllUsers;
 import starter.questions.SingleUser;
 import starter.tasks.GetAllUsers;
 import starter.tasks.GetSingleUser;
+import starter.util.StringToJson;
+
+import java.io.IOException;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 @Narrative(text = {"As a user",
         "I want to be able to ..",
         "in order to ..."})
+@DisplayName("Get users feature")
 public class GetUsers extends BaseTest {
 
+
+    /* Scenario:
+    * Given Pepe wants to buy a coffee
+    * When ...
+    * Then ...
+    * */
     @Test
-    @DisplayName("Get all the users registered")
+    @DisplayName("Get all the users registered from displayed name")
     @Tag("users")
+    @TestsRequirement("Given I want to buy" +
+            "When I buy " +
+            "Then I should ....")
     void getAllUsers() {
         actor.attemptsTo(
                 GetAllUsers.registered()
@@ -48,7 +62,7 @@ public class GetUsers extends BaseTest {
 
     @Test
     @DisplayName("Get a single user")
-    @Tag("users")
+    @Tag("users 2")
     void getASingleUser() {
         int userId = 2;
 
@@ -63,6 +77,15 @@ public class GetUsers extends BaseTest {
                 Ensure.that(singleUser.getData().getId()).isEqualTo(userId),
                 Ensure.that(singleUser.getData().getFirst_name()).isEqualTo("Janet")
         );
+    }
+
+    @Test
+    @DisplayName("Get a single user")
+    @Tag("users 2")
+    void getASingleUserParameterized() throws IOException {
+
+        StringToJson users = new StringToJson();
+        users.getUsers().forEach(System.out::println);
     }
 }
 
